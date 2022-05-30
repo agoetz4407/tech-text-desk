eed89143d5798de1f42deb869b5b2b5a4b037765
Andrew Goetz <agoetz4407@gmail.com>
2022-05-29 22:51:40 -0500

    model associations started, need to double check

--- models/index.js
+++ models/index.js
@@ -1,5 +1,29 @@
 const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
 //TODO: associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 
-module.exports = { User }
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+  
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
