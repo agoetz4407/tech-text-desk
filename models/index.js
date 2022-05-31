4acb08ef0d84e3abea1bd3d5053e8cc453fbca59
Andrew Goetz <agoetz4407@gmail.com>
2022-05-30 21:40:59 -0500

    added comment to post association

--- models/index.js
+++ models/index.js
@@ -18,6 +18,11 @@ Comment.belongsTo(User, {
     onDelete: 'SET NULL'
 });
 
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+  });
+
 User.hasMany(Comment, {
     foreignKey: 'user_id',
     onDelete: 'SET NULL'
