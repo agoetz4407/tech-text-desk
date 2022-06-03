d0689f3a932bb0e2abef20d09d43513d166860eb
Andrew Goetz <agoetz4407@gmail.com>
2022-06-03 15:26:34 -0500

    comment routes should be good

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,4 +2,60 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 const checkAuth = require('../../utils/checkAuth');
 
+router.get('/', (req, res) => {
+    Comment.findAll({
+        attributes: ['id', 'comment_text', 'created_at'],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Post,
+                attributes: ['title', 'post_text']
+            }
+        ]
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+router.post('/', checkAuth, (req, res) => {
+    if (req.session){
+        Comment.create({
+            comment_text: req.body.comment_text,
+            user_id: req.session.user_id,
+            post_id: req.body.post_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err)
+            res.status(400).json(err)
+        })
+    }
+})
+
+router.delete('/:id', checkAuth, (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({message:'No comment found with this id'});
+            return;
+        }
+        res.json(dbCommentData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    })
+})
+
 module.exports = router;
